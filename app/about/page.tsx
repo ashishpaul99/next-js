1da7c8a1b309c4f077ddebbca03fcd53c6e4c83e
ashishpaul99 <ashishpaul1199@gmail.com>
2026-01-02 22:36:16 +0530

    Intial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function About(){
+    return(
+        <>
+         <div>About</div>
+         <Link href="/">Link to Home Page</Link>
+        </>
+    )
+}
+
+
